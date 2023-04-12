5418994c3e4736b72f8e2922d6462553e8129073
Ben Cuan <github@bencuan.me>
2023-04-12 15:57:47 -0700

    more setup

--- Assets/RayMarchingCamera.cs
+++ Assets/RayMarchingCamera.cs
@@ -2,17 +2,38 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RayMarchingCamera : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+[RequireComponent(typeof(Camera))]
+[ExecuteInEditMode]
+public class RayMarchingCamera : MonoBehaviour {
+    [SerializeField]
+    private Shader _shader;
+
+    public Material _raymarchMaterial {
+        get {
+            if (!_raymarchMat && _shader) {
+                _raymarchMat = new Material(_shader);
+                _raymarchMat.hideFlags = HideFlags.HideAndDontSave;
+            }
+            return _raymarchMat;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    private Material _raymarchMat;
+
+    public Camera _camera {
+        get {
+            if (!_cam) {
+                _cam = GetComponent<Camera>();
+            }
+            return _cam;
+        }
+    }
+
+    private Camera _cam;
+
+    private void OnRenderImage(RenderTexture source, RenderTexture dest) {
+        if (!_raymarchMat) {
+            Graphics.Blit(source, dest);
+        }
     }
 }
